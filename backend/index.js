e1016dcec553c2dd71f2c1a87c3b91e71fdbca08
Jake McDermott <shakey1992jm@hotmail.co.uk>
2025-03-25 22:52:23 +0000

    commit update env

--- backend/index.js
+++ backend/index.js
@@ -91,6 +91,6 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server Listening on Port ${PORT}`);
