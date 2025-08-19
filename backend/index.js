cdba038d2c0ffc713d84a6dd5faeacf175f9c10d
Jake <shakey1992jm@hotmail.co.uk>
2025-08-19 02:35:45 +0100

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
 import express from "express";
 import morgan from "morgan";
-import cors from "cors";
 
 const app = express();
 // Middlewares
-app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"))
 morgan.token("body", (req) => JSON.stringify(req.body));
