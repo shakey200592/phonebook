cc60d33f3b48f3d4e1f42f527b804aff5142243f
Jake <shakey1992jm@hotmail.co.uk>
2025-08-19 02:29:21 +0100

    initial commit

--- frontend/src/services/persons.js
+++ frontend/src/services/persons.js
@@ -1,22 +1,19 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000/api/persons";
+const baseUrl = "/api/persons";
 
 export const getAll = () => {
   return axios.get(baseUrl);
 };
-
 export const create = (newPerson) => {
   return axios.post(baseUrl, newPerson);
 };
-
 export const update = (id, person, newNumber) => {
   return axios.put(`${baseUrl}/${id}`, {
     ...person,
     number: newNumber,
   });
 };
-
 export const remove = (id) => {
   return axios.delete(`${baseUrl}/${id}`);
 };
